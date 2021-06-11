13c69f0864ae63a4332baac7ceadb9683871ac04
Ana Paula <anapaulaauras@gmail.com>
2021-06-10 22:10:51 -0300

    Construindo compnentes.

---
+++ src/components/Comentario.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Comentario.css'
+
+//JSX (html dentro do JavaScrip)
+ const Comentario = () => (
+     <div className = "Comentario">
+     <div>João:</div>
+    <div>Olá, tudo bem?</div>
+    </div>
+ );
+
+ export default Comentario;
+
+
